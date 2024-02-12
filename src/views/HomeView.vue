0a177886115e2e3711da68f5e9f1d6d922b9b677
0x002500 <tiger_pan@outlook.com>
2024-02-12 20:24:59 +0800

    [feat] add a based button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <button><img class="red-packet" src="../../public/red-packet.png" alt="The picture for an red packet" /></button>
 </template>
+
+<style>
+  .red-packet {
+    width: 250px;
+  }
+</style>
