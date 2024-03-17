97f1faf141ca69837d364aceeb82e784193ccd00
0x002500 <tiger_pan@outlook.com>
2024-03-17 22:37:01 +0800

    [feat] ake it interesting but it is until very boring

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>a boring game</h1>
+    <a href="https://github.com/0x002500/red-packet">Click here to view the source code</a>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,37 @@
-<script setup>
-
-</script>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      count: 0,
-      time: 15,
-      type : 'A'
-    }
-  },
-  mounted() {
-    this.getCountdown();
-  },
-  methods: {
-    //add money to the total money when user click
-    add_money() {
-      this.count += Math.random() + 1;
-    },
-    getCountdown() {
-      let clock = window.setInterval(() => {
-        this.content = this.totalTime + 's后重新发送';
-        this.totalTime--;
-        if (this.totalTime < 0) {
-          this.type = 'C';
-          window.clearInterval(clock);
-        }
-      }, 1000);
-    }
-  }
-}
-</script>
-
 <template>
-  <button v-if="type === 'A'" @click="type = 'B'" class="start-button">现在开始！</button>
+  <button v-if="type === 'A'" @click="startGame" class="start-button">现在开始！</button>
   <h1 class="title" v-else-if="type === 'B'">现在你已获得{{ count.toFixed(2) }}元</h1>
   <h1 v-else> 结束！</h1>
   <h2 class="downclock">剩余时间：{{ time }}</h2>
-  <button @click="add_money" class="main-button" v-if="type === 'B'"><img src="/red-packet.png"
-      alt="The picture for an red packet" /></button>
+  <button @click="addMoney" class="main-button" v-if="type === 'B'"><img src="/red-packet.png"
+      alt="The picture for a red packet" /></button>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let count = ref(0);
+let time = ref(15);
+let type = ref('A');
+
+const addMoney = () => {
+    count.value += Math.random() + 1;
+};
+
+let clockInterval: number;
+
+const startGame = () => {
+  type.value = 'B';
+  const countdown = () => {
+    time.value--;
+    if (time.value < 0) {
+      type.value = 'C';
+      clearInterval(clockInterval);
+    }
+  };
+  clockInterval = window.setInterval(countdown, 1000);
+};
+</script>
 
 <style>
 .main-button {
@@ -62,10 +53,11 @@ export default {
   right: 5%;
   transform: translate(-50%, 0);
 }
+
 .start-button {
   margin: auto;
-    width: 50%;
-    padding: 10px;
+  width: 50%;
+  padding: 10px;
   transform: scale(1);
 }
-</style>
+</style>  
